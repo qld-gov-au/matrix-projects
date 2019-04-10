1d5c576282315cc1245fcbac38579f10ab3c4c29
Oliver Neal <oneal@squiz.net>
2019-04-10 12:24:29 +1000

    progress commits

---
+++ franchises/dfv/src/modules/support-services/js/global.js
@@ -0,0 +1,20 @@
+(function(){
+    'use strict';
+    
+    
+    /*
+        Functions
+    */
+
+
+    
+    
+    /*
+        Ready
+    */
+    
+    $(document).ready(function() {
+        var grid = document.querySelector('.qg-search-results__list');
+        salvattore.recreateColumns(grid);
+    });
+}());
