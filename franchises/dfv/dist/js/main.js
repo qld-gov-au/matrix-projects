d36b64c29783b4e9d145871b2c2fafb8b08748a1
Oliver Neal <oneal@squiz.net>
2019-04-23 09:28:36 +1000

    IE11 fixes

--- franchises/dfv/dist/js/main.js
+++ franchises/dfv/dist/js/main.js
@@ -675,37 +675,87 @@ process.umask = function() { return 0; };
 (function () {
   'use strict';
   /*
+      Polyfills for IE11
+  */
+  // MouseEvent doesn't exist in IE11
+
+  function checkForMouseEvent() {
+    try {
+      new MouseEvent('test');
+      return false; // No need to polyfill
+    } catch (e) {} // Need to polyfill - fall through
+    // Polyfills DOM4 MouseEvent
+
+
+    var MouseEventPolyfill = function MouseEventPolyfill(eventType, params) {
+      params = params || {
+        bubbles: false,
+        cancelable: false
+      };
+      var mouseEvent = document.createEvent('MouseEvent');
+      mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, params.screenX || 0, params.screenY || 0, params.clientX || 0, params.clientY || 0, params.ctrlKey || false, params.altKey || false, params.shiftKey || false, params.metaKey || false, params.button || 0, params.relatedTarget || null);
+      return mouseEvent;
+    };
+
+    MouseEventPolyfill.prototype = Event.prototype;
+    window.MouseEvent = MouseEventPolyfill;
+  } // Needed to bypass popup blockers
+
+
+  function simulateClick(elem) {
+    console.log('in simulateClick'); // Create our event (with options)
+
+    var evt = new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window
+    });
+    console.log('in simulateClick'); // If cancelled, don't dispatch our event
+
+    var canceled = !elem.dispatchEvent(evt);
+  }
+  /*
       Events
   */
+
 
   function keypressHandler(event) {
     // ESC
     if (event.keyCode === 27) {
-      closeWindow();
+      console.log('in keypressHandler');
+      var exit_link = document.querySelector('.qg-quick-exit__button');
+      simulateClick(exit_link);
     }
   }
-
-  document.addEventListener("keydown", keypressHandler, false);
   /*
       Functions
   */
   // Replace current window with new resource
 
-  function closeWindow() {
-    location.replace('https://www.google.com.au');
-    window.open('https://www.google.com.au', '_blank', '');
-  }
+
+  qg_dfv.fn.exitWindow = function () {
+    var new_location = 'https://www.google.com.au';
+    window.location.replace('https://www.google.com.au');
+    console.log('finish exitWindow');
+    /*setTimeout(function(){
+        window.open(new_location, '_blank', '');
+    }, 100);*/
+  };
   /*
       Ready
   */
+  // IE 11 polyfill
 
 
+  checkForMouseEvent(); // Standard events
+
+  document.addEventListener("keydown", keypressHandler, false);
   $(document).ready(function () {
     // IE 11 polyfill
     var sticky_elements = $('.qg-quick-exit__wrapper');
     Stickyfill.add(sticky_elements); // Binds
 
-    $('body').on('click', '.qg-quick-exit__button', closeWindow);
+    $('body').on('click', '.qg-quick-exit__button', qg_dfv.fn.exitWindow);
   });
 })();
 
