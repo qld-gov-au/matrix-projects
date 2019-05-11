7dbd2f6370af80bd540e13bbaf99b988b4f21f16
jchoi <jchoi@squiz.net>
2019-05-12 00:38:56 +1000

    Commit

--- services/src/modules/header-nav/js/global.js
+++ services/src/modules/header-nav/js/global.js
@@ -1,5 +1,49 @@
 (function(){
+
     'use strict';
+
+    var qg_main_nav_module = (function() {
+        
+        function setupMobileNav() {
+
+            var $site_header = $(".qg-site-header");
+            var $mobile_nav_toggle = $site_header.find(".qg-util-bar__mobile-nav-toggle");
+            
+            $mobile_nav_toggle.click(function(event) {
+                
+                var $this = $(event.target);
+
+                $site_header.toggleClass("qg-site-header--mobile-menu-open");
+
+            });
+
+        }
+
+        function init() {
+            
+            qg_main_nav.dom = {};
+
+            qg_main_nav.dom.$root = $(".qg-main-nav");
+            
+            if (qg_main_nav.dom.$root.length) {
+
+                setupMobileNav();
+                
+            }
+            
+        }
+        
+        var qg_main_nav = {};
+        
+        return {
+            init: init
+        }
     
     
+    }());
+    
+    document.addEventListener("DOMContentLoaded", function() {
+        qg_main_nav_module.init();
+    });
+
 }());
