d3650f7e0476d73dc0a2ece1810f03e86a6ab20a
Jason Choi <jchoi@squiz.net>
2019-06-24 15:38:07 +1000

    Rebuild

--- open-data/dist/js/main.js
+++ open-data/dist/js/main.js
@@ -6,7 +6,8 @@ __webpack_require__(1);
 __webpack_require__(3);
 __webpack_require__(4);
 __webpack_require__(5);
-module.exports = __webpack_require__(6);
+__webpack_require__(6);
+module.exports = __webpack_require__(7);
 
 
 /***/ }),
@@ -36,10 +37,47 @@ __webpack_require__.r(__webpack_exports__);
 /* 4 */
 /***/ (function(module, exports) {
 
+/**
+ * This will handle functionalities like
+ * - Expand all / Collapse all link
+ * - Ability to direct link to each section and expand the linked section
+ * - Handles aria-expanded values
+ */
+(function ($) {
+  var accordion = '.qg-accordion';
 
+  if ($(accordion).length > 0) {
+    var accordionControls = 'input[name=control]';
+    var linkedpanel = window.location.hash && $('input[aria-controls=' + window.location.hash.substring(1) + ']'); //Handle events of accordion inputs
+
+    $(accordion).find('article input').on('change', function () {
+      var checkedStatus = $(this).prop('checked');
+      var controlledPanedId = $('#' + $(this).attr('aria-controls'));
+      $(this).attr('aria-expanded', checkedStatus) //sets aria
+      .parents(accordion).find(accordionControls).prop('checked', false); //clears expand/collapse selection
+
+      controlledPanedId.attr('aria-hidden', !checkedStatus);
+    }); //expand all click
+
+    $(accordion).find(accordionControls).on('change', function () {
+      $(this).find('~ article input').prop('checked', $(this).val() === 'expand');
+      $(accordion).find('article input').trigger('change');
+    }); //Ability to direct link to each section and expand the linked section
+
+    if (linkedpanel.length > 0) {
+      linkedpanel.prop('checked', true);
+    }
+  }
+})(jQuery);
 
 /***/ }),
 /* 5 */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+/* 6 */
 /***/ (function(module, exports) {
 
 (function () {
@@ -135,7 +173,7 @@ __webpack_require__.r(__webpack_exports__);
 })();
 
 /***/ }),
-/* 6 */
+/* 7 */
 /***/ (function(module, exports) {
 
 
