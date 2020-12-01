77c6037828d84eff66f3b80e1cb400cd045dd3d6
asifamingov <asif.amin@dsiti.qld.gov.au>
2020-12-01 15:16:04 +1000

    Added js DC-17221-Accordions-fix

--- open-data/dist/js/main.js
+++ open-data/dist/js/main.js
@@ -47,103 +47,26 @@ __webpack_require__.r(__webpack_exports__);
   var accordion = '.qg-accordion';
 
   if ($(accordion).length > 0) {
-    var accItem = $(accordion).find('article');
-    var linkedpanel = $(window.location.hash); // keyboard accessibility
+    var accordionControls = 'input[name=control]';
+    var linkedpanel = window.location.hash && $('input[aria-controls=' + window.location.hash.substring(1) + ']'); //Handle events of accordion inputs
 
-    var a11yClick = function a11yClick(event) {
-      if (event.type === 'click') {
-        return true;
-      } else if (event.type === 'keypress') {
-        var code = event.charCode || event.keyCode;
+    $(accordion).find('article input').on('change', function () {
+      var checkedStatus = $(this).prop('checked');
+      var controlledPanedId = $('#' + $(this).attr('aria-controls'));
+      $(this).attr('aria-expanded', checkedStatus) //sets aria
+      .parents(accordion).find(accordionControls).prop('checked', false); //clears expand/collapse selection
 
-        if (code === 32 || code === 13) {
-          return true;
-        }
-      } else {
-        return false;
-      }
-    }; //Handle events of accordion inputs
-
-
-    $(accordion).find('article input[name=tabs]').on('change', function () {
-      var checkedStatus = $(this).prop('checked'); // let controlledPanedId = $('#' + $(this).attr('aria-controls'));
-
-      $(this).attr('aria-expanded', checkedStatus); //clears expand/collapse selection
-
-      $(this).parent('article').find('.collapsing-section').attr('aria-hidden', !checkedStatus);
-    }); // open on page load
-
-    var hashTrigger = function hashTrigger() {
-      if (linkedpanel.length > 0) {
-        $(linkedpanel).parent(accordion).find('~ article input').prop('checked', false); //clears expand/collapse selection
-
-        $(linkedpanel).prop('checked', true).attr('aria-expanded', 'true');
-        $(linkedpanel).parent('article').find('.collapsing-section').attr('aria-hidden', 'false');
-        $('html, body').animate({
-          'scrollTop': $(linkedpanel).offset().top
-        }, 500);
-      }
-    };
-
-    hashTrigger();
-    window.onhashchange = hashTrigger;
-    $('input[name=tabs]').click(function () {
-      $(this).parent('article').find('.acc-heading').focus();
-    }); // highlight title on hover
-
-    accItem.hover(function () {
-      $(accordion).find('.title').removeClass('ht');
-      $(this).find('.title').addClass('ht');
-    }, function () {
-      $(accordion).find('.title').removeClass('ht');
-    }); // expand/collapse on enter keypress
-
-    accItem.find('.acc-heading').on('keypress', function (event) {
-      if (event.target === event.currentTarget) {
-        event.preventDefault();
-
-        if (a11yClick(event) === true) {
-          var parent = $(this).parent();
-
-          if (parent.find('input[name="tabs"]:checked').length > 0) {
-            parent.find('input[name="tabs"]').prop('checked', false);
-          } else {
-            parent.find('input[name="tabs"]').prop('checked', true);
-          }
-        }
-      }
-    });
-    accItem.find('.acc-heading').on('click', function (event) {
-      if (event.target === event.currentTarget) {
-        if (event.clientX !== 0) {
-          var parent = $(this).parent();
-
-          if (parent.find('input[name="tabs"]:checked').length > 0) {
-            parent.find('input[name="tabs"]').prop('checked', false);
-          } else {
-            parent.find('input[name="tabs"]').prop('checked', true);
-          }
-
-          return false;
-        }
-      }
+      controlledPanedId.attr('aria-hidden', !checkedStatus);
     }); //expand all click
-    // label selector is to provide backward compatibility in case projects are using old markup
 
-    $('.qg-acc-controls .expand, label[for=\'expand\']').on('click keypress', function (event) {
-      if (a11yClick(event) === true) {
-        $(this).parents('.qg-accordion').find('input:checkbox').prop('checked', true);
-        event.preventDefault();
-      }
-    }); // collapse all click
-    // label selector is to provide backward compatibility in case projects are using old markup
+    $(accordion).find(accordionControls).on('change', function () {
+      $(this).find('~ article input').prop('checked', $(this).val() === 'expand');
+      $(accordion).find('article input').trigger('change');
+    }); //Ability to direct link to each section and expand the linked section
 
-    $('.qg-acc-controls .collapse, label[for=\'collapse\']').on('click keypress', function (event) {
-      if (a11yClick(event) === true) {
-        $(this).parents('.qg-accordion').find('input:checkbox').prop('checked', false);
-        event.preventDefault();
-      }
-    });
+    if (linkedpanel.length > 0) {
+      linkedpanel.prop('checked', true);
+    }
   }
 })(jQuery);
 
@@ -163,11 +86,11 @@ __webpack_require__.r(__webpack_exports__);
    * =================================
    * Contact Landing Page Form Module
    * =================================
-   * 
+   *
    * Depending on what radio the user selects on the contact landing page form, the user could either
    * - be redirected to a different URL or
    * - see an alert
-   * 
+   *
    */
 
   var qg_open_data_contact_lp_form_module = function () {
