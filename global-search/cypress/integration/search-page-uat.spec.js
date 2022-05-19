0676cc9f9b97a9ebd19c877ee5556411c269b655
asifamingov <asif.amin@dsiti.qld.gov.au>
2022-05-19 14:55:03 +1000

    updated script name

--- global-search/cypress/integration/search-page-uat.spec.js
+++ global-search/cypress/integration/search-page-uat.spec.js
@@ -1,6 +1,17 @@
+import "../support/commands";
+
 const ROOT_URL = 'https://oss-uat.clients.squiz.net/search'
 
+
 context("Search page", () => {
+    beforeEach(() => {
+        const totalResults = function (){
+            cy.get('.qg-search-results__results-total').then(($element) => {
+                return $element.text().replace(/,/g, '');
+            });
+        }
+    });
+
     it("Filter should not be visible if scope or profile are not present in the URL", () => {
         cy.visit(`${ROOT_URL}/_nocache`)
         cy
@@ -26,10 +37,12 @@ context("Search page", () => {
     it("Select a radio button to determine the number of results", () => {
         cy.get('input[name=filterBy][value=\'qld\']').first().check()
         cy.get('#applyFilter').click()
-        cy.get('.qg-search-results__results-count').contains('25,895')
+        cy.get('.qg-search-results__results-total').then(($element) => {
+            return $element.text().replace(/,/g, '');
+        }).then(parseFloat).should('be.eq', 25902);
         cy.get('input[name=filterBy][value=\'custom\']').first().check()
         cy.get('#applyFilter').click()
-        cy.get('.qg-search-results__results-count').contains('1,411')
+        cy.get('.qg-search-results__results-count').contains('1,418')
     })
 
     // profile is present with no scope
@@ -44,9 +57,11 @@ context("Search page", () => {
     it("Check results by changing filters", () => {
         cy.get('input[name=filterBy][value=\'qld\']').first().check()
         cy.get('#applyFilter').click()
-        cy.get('.qg-search-results__results-count').contains('25,895')
+        cy.get('.qg-search-results__results-total').then(($element) => {
+            return $element.text().replace(/,/g, '');
+        }).then(parseFloat).should('be.eq', 25902);
         cy.get('input[name=filterBy][value=\'custom\']').first().check()
         cy.get('#applyFilter').click()
-        cy.get('.qg-search-results__results-count').contains('1,444')
+        cy.get('.qg-search-results__results-count').contains('1,445')
     })
 })
