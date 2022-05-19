1735d04f7ff93a17b7154aeb12d946eaea59983e
asifamingov <asif.amin@dsiti.qld.gov.au>
2022-05-19 15:29:44 +1000

    test

---
+++ squiz-matrix-regression-testing/cypress/support/index.js
@@ -0,0 +1,24 @@
+import '@percy/cypress'
+
+const ROOT_URL = 'https://oss-uat.clients.squiz.net'
+
+describe('Homepage snapshot', function() {
+    it('Loads the homepage', function() {
+            cy.visit(ROOT_URL);
+            cy.percySnapshot();
+     });
+});
+
+describe('Search page snapshot', function() {
+    it('Loads the Search page', function() {
+        cy.visit(`${ROOT_URL}/search`);
+        cy.percySnapshot();
+    });
+});
+
+describe('Services page snapshot', function() {
+    it('Loads the Services page', function() {
+        cy.visit(`${ROOT_URL}/services`);
+        cy.percySnapshot();
+    });
+});
